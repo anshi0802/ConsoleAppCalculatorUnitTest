93be0b57c7296b9bbb7bbfea336567d4b51ebd80
Anshi Musthafa <anshiey.anu@gmail.com>
2024-12-18 10:33:26 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using ClassLibraryBusiness;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppCalculatorUnitTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+            int result = calculator.Add(100, 200);
+            Console.WriteLine("Sum Of Two Numbers Are:");
+            Console.WriteLine(result);
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Subtraction Of Two Numbers Are:");
+            int Sub = calculator.Subtract(300, 200);
+            Console.WriteLine(Sub);
+            Console.ReadLine();
+
+
+        }
+    }
+}
